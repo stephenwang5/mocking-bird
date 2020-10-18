7031a507790e3b6d6befe11695ce24734a360163
Code-Titan <artemis.sotnikov12@gmail.com>
2020-10-18 15:42:59 -0400

    Add Several Features

---
+++ twitter_retrieval.py
@@ -0,0 +1,35 @@
+#taken from https://stackabuse.com/accessing-the-twitter-api-with-python/ for the purpose of testing
+
+# Import the Twython class
+from twython import Twython
+import json
+
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+# Create our query
+query = {'q': 'trump',
+        'result_type': 'popular',
+        'count': 10,
+        'lang': 'en',
+        }
+
+
+import pandas as pd 
+
+# Search tweets
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in python_tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)
+df.sort_values(by='favorite_count', inplace=True, ascending=False)
+df.head(5)
