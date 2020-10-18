fee0b57bd06b521a33949e56d86a0d45dce9efbf
Stephen Wang <ste53539@gmail.com>
2020-10-18 17:20:41 -0400

    more wip

--- mock.js
+++ mock.js
@@ -1,8 +1,18 @@
+chrome.storage.sync.get(["handles"], result => {
+    result_list = result["handles"].split('\n');
+    result_for_command = "";
+    for (itm of result) {
+        result_for_command += itm;
+    }
 
-fetch("http://52.154.72.216:6942?trump&Biden")
-.then(res=>res.text())
-// .then(obj=>{document.getElementById("tweet").innerHTML = "<p>"+obj+"<p>";})
-.then(obj=>{document.getElementById("tweet").innerHTML = obj; console.log("here's what: " + obj);})
-.catch(err=>{console.error(err);});
+    fetch_command = "http://52.154.72.216:6942?" + result_for_command
 
-// document.getElementById("tweet").innerHTML += "<p>random</p>";
+    //fetch(fetch_command)
+    fetch("http://52.154.72.216:6942?trump&Biden")
+    .then(res=>res.text())
+    .then(obj=>{
+        document.getElementById("tweet").innerHTML = obj;
+        // console.log("here's what: " + obj);
+    })
+    .catch(err=>{console.error(err);});
+})
