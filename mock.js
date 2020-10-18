e0f87585ce879301a462488e2481f89c40122ee1
Stephen Wang <ste53539@gmail.com>
2020-10-18 17:59:48 -0400

    finished user list

--- mock.js
+++ mock.js
@@ -1,14 +1,16 @@
 chrome.storage.sync.get(["handles"], result => {
     result_list = result["handles"].split('\n');
     result_for_command = "";
-    for (itm of result) {
-        result_for_command += itm;
+    for (itm of result_list) {
+        result_for_command += itm + '&';
     }
+    result_for_command = result_for_command.substr(0, result_for_command.length-1);
 
-    fetch_command = "http://52.154.72.216:6942?" + result_for_command
+    let fetch_command = "http://52.154.72.216:6942?" + result_for_command;
+    console.log(fetch_command);
 
-    //fetch(fetch_command)
-    fetch("http://52.154.72.216:6942?trump&Biden")
+    fetch(fetch_command)
+    // fetch("http://52.154.72.216:6942?realDonalTrump&JoeBiden")
     .then(res=>res.text())
     .then(obj=>{
         document.getElementById("tweet").innerHTML = obj;
